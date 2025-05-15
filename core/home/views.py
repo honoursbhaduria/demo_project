8e1fad5adcdc8c25a5f410cd1d7433383652691d
honours <honoursbhadauria@gmail.com>
2025-05-16 01:09:57 +0530

    Refactor home view and update URL routing for improved API response

--- core/home/views.py
+++ core/home/views.py
@@ -1,17 +1,11 @@
+ 
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
-
-# Create your views here.
-@api_view
+@api_view(['GET'])
 def home(request):
     return Response({
-        'status' : 200 , 'meassage' : 'Welcome to the API' , 'data' : {
-            'name' : 'Django Rest Framework API',
-            'version' : '1.0.0',
-            'author' : 'Your Name'
-        }
+        'status': 200,
+        'message': 'Welcome to the API'  
     })
-
-    
