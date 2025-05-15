1f8c4483d85b7b55fd21f70572c2a058cc5da496
honours <honoursbhadauria@gmail.com>
2025-05-16 00:57:52 +0530

    Add URL routing for home view and implement home API response

--- core/home/views.py
+++ core/home/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+
 
 # Create your views here.
+@api_view
+def home(request):
+    return Response({
+        'status' : 200 , 'meassage' : 'Welcome to the API' , 'data' : {
+            'name' : 'Django Rest Framework API',
+            'version' : '1.0.0',
+            'author' : 'Your Name'
+        }
+    })
+
+    
